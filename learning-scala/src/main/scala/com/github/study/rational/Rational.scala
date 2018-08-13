bd6b1327ae4b01c61f1276a4a820855a8d8169b2
marcyyy0612 <marcyyy0612@gmail.com>
2018-08-13 10:12:50 +0900

    implement '=' identifier

--- learning-scala/src/main/scala/com/github/study/rational/Rational.scala
+++ learning-scala/src/main/scala/com/github/study/rational/Rational.scala
@@ -5,9 +5,9 @@ case class Rational(n: Int, d: Int) {
   def +(that: Rational): Rational =
     (that.n, that.d) match {
       case (n, d) if (this.d == d) =>
-        new Rational(this.n + n, d)
+        Rational(this.n + n, d)
       case (n, d) if (this.d != d) =>
-        new Rational(this.n * d + n * this.d, this.d * d)
+        Rational(this.n * d + n * this.d, this.d * d)
     }
 
   def <(that: Rational): Boolean =
@@ -32,5 +32,11 @@ case class Rational(n: Int, d: Int) {
     (that.n, that.d) match {
       case (n, d) if (this.n / this.d >= n / d) => true
       case _                                    => false
+    }
+
+  def ==(that: Rational): Boolean =
+    (that.n, that.d) match {
+      case (n, d) if (this.n == this.d) => true
+      case _                            => false
     }
 }
