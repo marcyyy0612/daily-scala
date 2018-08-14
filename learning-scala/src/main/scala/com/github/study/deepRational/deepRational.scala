b0f3dae9fe3cd5c5617f6f90f956057c6bf0f160
marcyyy0612 <marcyyy0612@gmail.com>
2018-08-15 08:32:18 +0900

    wip deepRational

--- learning-scala/src/main/scala/com/github/study/deepRational/deepRational.scala
+++ learning-scala/src/main/scala/com/github/study/deepRational/deepRational.scala
@@ -1,17 +1,18 @@
-trait Number {
-  def value: Int
+trait Base {
+  def +(value: Int): Int
 }
 
-case class Integer(num: Int) extends Number {
-  
+case class Number(value: Int) extends Base {
+  def +(that: Base): Int = 
+    this.value + that.value
 }
 
-case class Rational(n: Number, p: Number) extends Number {
-
-}
-
-case class Calc(value: Number) {
-  def +(value: Number) = {
-
-  }
+case class Rational(n: Base, d: Base) extends Base {
+  def +(that: Base): Base =
+    (that.n, that.d) match {
+      case (n, d) if (this.d == d) =>
+        Rational(this.n + n, d)
+      case (n, d) if (this.d != d) =>
+        Rational(this.n * d + n * this.d, this.d * d)
+    }
 }
